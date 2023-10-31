754032baca637d2f17dbe4dd43f8c222c7dc9eaa
Clarke Lamothe <clarkelamothe@yahoo.fr>
2023-10-31 09:19:35 -0300

    detail designed

--- app/src/main/java/com/clarkelamothe/recetapp/recipe/domain/model/Recipe.kt
+++ app/src/main/java/com/clarkelamothe/recetapp/recipe/domain/model/Recipe.kt
@@ -5,6 +5,9 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Recipe(
-    val imageUrl: String,
-    val name: String
+    val imageUrl: String = "",
+    val name: String = "",
+    val description: String = "",
+    val location: String = "",
+    val items: List<String> = emptyList()
 ) : Parcelable
