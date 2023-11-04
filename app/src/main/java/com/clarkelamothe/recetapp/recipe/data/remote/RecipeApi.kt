a13a565eb8407c7141123182b61bb99695d24c77
Clarke Lamothe <clarkelamothe@yahoo.fr>
2023-11-04 17:18:26 -0300

    new mock link RecipeApi

--- app/src/main/java/com/clarkelamothe/recetapp/recipe/data/remote/RecipeApi.kt
+++ app/src/main/java/com/clarkelamothe/recetapp/recipe/data/remote/RecipeApi.kt
@@ -4,9 +4,8 @@ import com.clarkelamothe.recetapp.core.data.model.RecipeResponse
 import retrofit2.Response
 import retrofit2.http.GET
 
-const val RECIPE = "e701f374-3806-42a0-b267-7ba7ef550983"
 interface RecipeApi {
-    //    @GET("https://run.mocky.io/v3/e701f374-3806-42a0-b267-7ba7ef550983?mocky-delay=60000ms")
-    @GET(RECIPE)
-    suspend fun getRecipes(): Response<RecipeResponse<List<RecipeDto>>>
+//    @GET("https://demo2426464.mockable.io/recipes")
+@GET("https://run.mocky.io/v3/3c35f18a-8b82-4b23-bde1-eba3bdba3bf0")
+suspend fun getRecipes(): Response<RecipeResponse<List<RecipeDto>>>
 }
